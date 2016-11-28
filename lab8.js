99ba7ade6baa884bada29641ab972c66080f30c4
Joshua Anderson <j@joshua-anderson.com>
2016-11-28 14:26:40 -0800

    Basic lab7

--- lab8.js
+++ lab8.js
@@ -9,29 +9,60 @@ function preload() {
 function setup() {
   var canvas = createCanvas(260*2, 330*2);
   canvas.parent('canvas');
+  img1.loadPixels();
+  bgnd(img1,color(255,0,0));
+  shrt(img1,color(0,255,0));
+  logo(img1,color(0,0,255));
+  img1.updatePixels();
   image(img1,0,0);
   image(img2,260,0);
   image(img3,0,330);
   image(img4,260,330);
 }
 
+function bgnd(img, c) {
+  for(var x = 0; x < 260; x++){
+    for(var y = 0; y<250; y++) {
+      if(x>100&&x<160&&y>50)
+        continue;
+      var px = img.get(x,y);
+      if(allgtr(px,130)) {
+        img.set(x,y,c);
+      }
+    }
+  }
+}
 
+function allgtr(px,n){
+  if(px[0]>n && px[1]>n && px[2]>n)
+    return true
+  return false
+}
 
-//  img1.loadPixels();
-//    img2.loadPixels();
-///* 2D loop to show how to loop in 2D into a 1D array */
-//for( var y=0; y < img1.height; y++) {
-//for (var x=0; x < img1.width; x++) {
-///*map from 2D to 1D */
-//loc = img2.width*y + x;
-////for every other x, copy in the pixel to make stripes
-//if (x%2 ==0) {
-//img2.set(x, y, [255, 255, 255, 255]);
-//}
-//}
-//}
-//img2.updatePixels();
-//}
-//function draw() {
-//image(img2, 0, 0);
-//}
+function allls(px,n){
+  if(px[0]<n && px[1]<n && px[2]<n)
+    return true
+  return false
+}
+
+function shrt(img, c) {
+  for(var x = 0; x < 260; x++){
+    for(var y = 200; y<330; y++) {
+      var px = img.get(x,y);
+      if(allls(px,120)) {
+        img.set(x,y,c);
+      }
+    }
+  }
+}
+
+function logo(img, c) {
+  for(var x = 180; x < 220; x++){
+    for(var y = 310; y<330; y++) {
+      var px = img.get(x,y);
+      if(px[2]>150) {
+        img.set(x,y,c);
+      }
+    }
+  }
+}
